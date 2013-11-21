16c7846761e7216ed57f5a62e185780571e1c85c
frostib <frostib@hotmail.com>
2013-11-21 16:36:52 +0000

    Update tictactoetest.java
    bool

--- src/test/java/tictactoe/tictactoetest.java
+++ src/test/java/tictactoe/tictactoetest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertEquals;
 public class tictactoetest
 {
     @Test
-    public void boardsize() throws Exception
+    public boolean boardsize() throws Exception
     {
         Board testsize = new Board();
         assertEquals(testsize.drawBoard(600,600),true);
