8173e85e529240b64100242fa513b170cdff07b8
dannice <daniela12@ru.is>
2013-11-21 15:05:23 +0000

    Update tictactoe.java

--- src/main/java/tictactoe/tictactoe.java
+++ src/main/java/tictactoe/tictactoe.java
@@ -1,5 +1,11 @@
 public class tictactoe
 {	
+	public static void main(String[] args) {
+        
+        tictactoe newgame = new tictactoe();
+        newgame.playgame();
+        
+    }
 	public void playgame() 
 	{
         Board game = new Board();
