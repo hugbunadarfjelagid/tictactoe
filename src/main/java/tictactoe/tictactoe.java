2ce163401a3fd73b40dcddc971a82c2b53469b28
frostib <frostib@hotmail.com>
2013-11-21 22:30:06 +0000

    Update tictactoe.java

--- src/main/java/tictactoe/tictactoe.java
+++ src/main/java/tictactoe/tictactoe.java
@@ -17,6 +17,27 @@ public class tictactoe
         prepareBoard();
         preparePlayers();
         game.markGrid(Board);
+                    while ((!P1.won && !P2.won ) && (moves < 9)){
+                if (StdDraw.mousePressed() || testing) {
+                    if (!testing){
+                    x = StdDraw.mouseX();
+                    y = StdDraw.mouseY();}
+                    else {
+                        x = StdRandom.uniform(0.2, 0.8);
+                        y = StdRandom.uniform(0.2, 0.8);
+                    }
+                    game.makeMoveMouse(x,y,P1,Board);
+                    switchPl(P1,P2);
+                    game.DrawToBoard(Board);
+                    checkWin(P1,P2,Board);
+                    moves++;
+
+                }
+            }
+            if(P1.won){System.out.println("Player X won");}
+            else if(P2.won){System.out.println("Player O won");}
+            else{System.out.println("Draw");}
+                System.exit(0);
 	}
 	
 	public void prepareBoard() {
@@ -39,6 +60,44 @@ public class tictactoe
         	P2.won = false;
 	}
 	
+	    public void checkWin(Players P1, Players P2, Field[] board) {
+        if (board[0].getSlot()==1 && board[4].getSlot()==1 && board[8].getSlot()==1)
+            P1.won = true;
+        if (board[2].getSlot()==1 && board[4].getSlot()==1 && board[6].getSlot()==1)
+            P1.won = true;
+        if (board[0].getSlot()==1 && board[3].getSlot()==1 && board[6].getSlot()==1)
+            P1.won = true;
+        if (board[1].getSlot()==1 && board[4].getSlot()==1 && board[7].getSlot()==1)
+            P1.won = true;
+        if (board[2].getSlot()==1 && board[5].getSlot()==1 && board[8].getSlot()==1)
+            P1.won = true;
+        if (board[0].getSlot()==1 && board[1].getSlot()==1 && board[2].getSlot()==1)
+            P1.won = true;
+        if (board[3].getSlot()==1 && board[4].getSlot()==1 && board[5].getSlot()==1)
+            P1.won = true;
+        if (board[6].getSlot()==1 && board[7].getSlot()==1 && board[8].getSlot()==1)
+            P1.won = true;
+        if (board[0].getSlot()==2 && board[4].getSlot()==2 && board[8].getSlot()==2)
+            P2.won = true;
+        if (board[2].getSlot()==2 && board[4].getSlot()==2 && board[6].getSlot()==2)
+            P2.won = true;
+        if (board[0].getSlot()==2 && board[3].getSlot()==2 && board[6].getSlot()==2)
+            P2.won = true;
+        if (board[1].getSlot()==2 && board[4].getSlot()==2 && board[7].getSlot()==2)
+            P2.won = true;
+        if (board[2].getSlot()==2 && board[5].getSlot()==2 && board[8].getSlot()==2)
+            P2.won = true;
+        if (board[0].getSlot()==2 && board[1].getSlot()==2 && board[2].getSlot()==2)
+            P2.won = true;
+        if (board[3].getSlot()==2 && board[4].getSlot()==2 && board[5].getSlot()==2)
+            P2.won = true;
+        if (board[6].getSlot()==2 && board[7].getSlot()==2 && board[8].getSlot()==2)
+            P2.won = true;
+    }
 	
+	   public void switchPl(Players P1, Players P2) {
+        P1.changeTurn();
+        P2.changeTurn();
+    }
 	
 }
