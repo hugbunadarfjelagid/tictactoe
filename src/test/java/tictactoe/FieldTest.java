c2f0bb53bac44e77679289ff6047c7f7a66263b0
frostib <frostib@hotmail.com>
2013-11-21 16:56:21 +0000

    Create FieldTest.java

---
+++ src/test/java/tictactoe/FieldTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class FieldTest
+{
+    @Test
+    public void EmptySlot() throws Exception
+    {
+        Slot test = new Slot();
+        assertEquals(test.Slot(0),mark.Slot(0));
+    }
+}
