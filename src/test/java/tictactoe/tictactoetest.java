4433ca74641ff9090bf964fd6e0e757d4de50cc7
frostib <frostib@hotmail.com>
2013-11-21 16:34:15 +0000

    Update tictactoetest.java
    throws Exception!

--- src/test/java/tictactoe/tictactoetest.java
+++ src/test/java/tictactoe/tictactoetest.java
@@ -1,11 +1,11 @@
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+//import static org.junit.Assert.assertTrue;
 
 public class tictactoetest
 {
     @Test
-    public void boardsize()
+    public void boardsize() throws Exception
     {
         Board testsize = new Board();
         assertEquals(testsize.drawBoard(600,600),true);
