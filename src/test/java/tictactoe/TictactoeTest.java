6d30023c23b8fb5ca04aecfcf6f8a4b393bca030
frostib <frostib@hotmail.com>
2013-11-22 13:59:28 +0000

    Update TictactoeTest.java

--- src/test/java/tictactoe/TictactoeTest.java
+++ src/test/java/tictactoe/TictactoeTest.java
@@ -7,13 +7,13 @@ public class TictactoeTest
     public void LabelSpaces() throws Exception
     {
     	Space[] stest = new Space[9];
-		int space = 0; // Initialize counter for spaces
-		// Create placeholder for each space on the gameboard
-		while(space < 9)
-		{
-			Board[space] = new Space();
-			space++;
-		}
+    	Board btest = new Board();
+	int space = 0;
+	while(space < 9)
+	{
+		Board[space] = new Space();
+		space++;
+	}
     	int counter = 0;
     	for (int i = 0; i < 9; i++)
     	{
