f5bea2c2143492ac74767a1ef727ff502a839ddd
frostib <frostib@hotmail.com>
2013-11-22 13:57:18 +0000

    Update TictactoeTest.java

--- src/test/java/tictactoe/TictactoeTest.java
+++ src/test/java/tictactoe/TictactoeTest.java
@@ -4,15 +4,22 @@ import static org.junit.Assert.assertEquals;
 public class TictactoeTest
 {
     @Test
-    public void EmptyBoard() throws Exception
+    public void LabelSpaces() throws Exception
     {
     	Space[] stest = new Space[9];
+		int space = 0; // Initialize counter for spaces
+		// Create placeholder for each space on the gameboard
+		while(space < 9)
+		{
+			Board[space] = new Space();
+			space++;
+		}
     	int counter = 0;
     	for (int i = 0; i < 9; i++)
     	{
     		stest[i].getSpace();
-    		counter += i;
+    		counter = i;
     	}
-        assertEquals(counter,0);
+        assertEquals(counter,8);
     }
 }
