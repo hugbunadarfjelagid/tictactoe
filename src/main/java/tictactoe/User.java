b78e98da03ed39833356488bafccce11d5165694
frostib <frostib@hotmail.com>
2013-11-22 13:01:29 +0000

    Cleanup and comment code

--- src/main/java/tictactoe/User.java
+++ src/main/java/tictactoe/User.java
@@ -1,28 +1,30 @@
-//package main.java.tictactoe;
 public class User
 {
-//    public int moves = 0;
     public boolean isTurn = false;
-    private String name /* = ""*/;
+    private String name;
     public boolean won = false;
 
+    // constructor
     public void Player(String name, boolean turn)
     {
         this.name = name;
         this.isTurn = turn;
     }
 
+    // set name
     public String setName(String name)
     {
         this.name = name;
         return this.name;
     }
 
+    // get name
     public String getName()
     {
         return this.name;
     }
 
+    // change players turn
     public void changeTurn()
     {
         if (this.isTurn == false)
@@ -30,6 +32,8 @@ public class User
         else
             this.isTurn = false;
     }
+
+    // who's turn it is
     public boolean isTurn(boolean turn)
     {
         this.isTurn = turn;
