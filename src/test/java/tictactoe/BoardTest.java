e675cb1186a67cc255128480cd6c4a4fb7ffb082
frostib <frostib@hotmail.com>
2013-11-22 13:44:27 +0000

    Update BoardTest.java

--- src/test/java/tictactoe/BoardTest.java
+++ src/test/java/tictactoe/BoardTest.java
@@ -1,4 +1,4 @@
-import org.junit.Test;
+/*import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class BoardTest
@@ -15,4 +15,4 @@ public class BoardTest
     	}
         assertEquals(counter,0);
     }
-}
+}*/
