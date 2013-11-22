94591a5243ed040a4d00beae54164a6a4d541197
frostib <frostib@hotmail.com>
2013-11-22 06:28:14 +0000

    Create User.java

---
+++ src/main/java/tictactoe/User.java
@@ -0,0 +1,38 @@
+package main.java.tictactoe;
+public class User
+{
+//    public int moves = 0;
+    public boolean isTurn = false;
+    private String name /* = ""*/;
+    public boolean won = false;
+
+    public void Player(String name, boolean turn)
+    {
+        this.name = name;
+        this.isTurn = turn;
+    }
+
+    public String setName(String name)
+    {
+        this.name = name;
+        return this.name;
+    }
+
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public void changeTurn()
+    {
+        if (this.isTurn == false)
+            this.isTurn = true;
+        else
+            this.isTurn = false;
+    }
+    public boolean isTurn(boolean turn)
+    {
+        this.isTurn = turn;
+        return isTurn;
+    }
+}
