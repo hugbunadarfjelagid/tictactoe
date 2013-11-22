adda5b38238ed2bffaf7808bfc60dd359de5dcb2
frostib <frostib@hotmail.com>
2013-11-22 13:44:00 +0000

    Update SpaceTest.java

--- src/test/java/tictactoe/SpaceTest.java
+++ src/test/java/tictactoe/SpaceTest.java
@@ -11,4 +11,18 @@ public class SpaceTest
         ptest.assigned = 0;
         assertEquals(stest.place(ptest),true);
     }
+    
+    @Test
+    public void EmptyBoardTest() throws Exception
+    {
+    	Space stest = new Space();
+    	int counter = 0;
+    	for (int i = 0; i < 9; i++)
+    	{
+    		stest[i].getSpace();
+    		counter += i;
+    	}
+        assertEquals(counter,0);
+    }
 }
+
