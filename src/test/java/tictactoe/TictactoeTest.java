3d14ddd9b64c2da654736b55cef2a83988640d3a
frostib <frostib@hotmail.com>
2013-11-22 14:08:31 +0000

    Update TictactoeTest.java

--- src/test/java/tictactoe/TictactoeTest.java
+++ src/test/java/tictactoe/TictactoeTest.java
@@ -7,13 +7,12 @@ public class TictactoeTest
     public void NrOfSpacesTest() throws Exception
     {
     	Space[] stest = new Space[9];
-   // 	Board btest = new Board();
 	int space = 0;
 	while(space < 9)
 	{
 		stest[space] = new Space();
 		space++;
 	}
-        assertEquals(space,8);
+        assertEquals(space,9);
     }
 }
