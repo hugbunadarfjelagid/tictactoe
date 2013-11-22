684bebf359d653ebc8746e08a65bd68ea35d6c15
frostib <frostib@hotmail.com>
2013-11-22 01:00:51 +0000

    Update Field.java

--- src/main/java/tictactoe/Field.java
+++ src/main/java/tictactoe/Field.java
@@ -1,24 +1,32 @@
-public class Field {
+public class Field
+{
+    private boolean taken;
+    public Slot mark = new Slot();
 
-        private boolean taken;
-        public Slot mark = new Slot();
-
-        private Boolean taken() {
-           return taken;
-        }
-    public void setSlot(Slot mark) {
+    private Boolean taken()
+    {
+        return taken; // Taken slot
+    }
+    
+    public void setSlot(Slot mark)
+    {
         this.mark = mark;
     }
-        public int getSlot() {
-            return mark.Slot;
-        }
 
-        public boolean place(Slot x) {
-            if (!(taken)) {
+    public int getSlot()
+    {
+        return mark.Slot;
+    }
+
+    // Mark slot if it's not taken
+    public boolean place(Slot x)
+    {
+        if (!(taken))
+        {
             mark = x;
             taken = true;
             return taken;
         }
-        return false;
-        }
+    return false;
+    }
 }
