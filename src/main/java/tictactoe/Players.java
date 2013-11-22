241b1f161a76b854fe5e9dad61183f32efaaffcd
frostib <frostib@hotmail.com>
2013-11-22 01:01:25 +0000

    Update Players.java

--- src/main/java/tictactoe/Players.java
+++ src/main/java/tictactoe/Players.java
@@ -1,27 +1,37 @@
-public class Players {
-    public int moves = 0;
+public class Players
+{
+//    public int moves = 0;
     public boolean isTurn = false;
-    private String name = "";
+    private String name /* = ""*/;
     public boolean won = false;
 
-    public void Player(String name, boolean turn){
+    public void Player(String name, boolean turn)
+    {
         this.name = name;
         this.isTurn = turn;
     }
-    public String setName(String name) {
+
+    public String setName(String name)
+    {
         this.name = name;
         return this.name;
     }
-    public String getName(){
+
+    public String getName()
+    {
         return this.name;
     }
-    public void changeTurn() {
+
+    public void changeTurn()
+    {
         if (this.isTurn == false)
             this.isTurn = true;
-        else this.isTurn = false;
+        else
+            this.isTurn = false;
     }
-    public boolean isTurn(boolean turn){
+    public boolean isTurn(boolean turn)
+    {
         this.isTurn = turn;
         return isTurn;
     }
-    }
+}
