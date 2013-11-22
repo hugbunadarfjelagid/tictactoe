dcd497cfa3bf16d8a1b081cdbbd3892089336a0e
frostib <frostib@hotmail.com>
2013-11-22 13:33:20 +0000

    Create BoardTest.java

---
+++ src/test/java/tictactoe/BoardTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class BoardTest
+{
+    @Test
+    public void EmptyBoard() throws Exception
+    {
+    	int counter = 0;
+    	for (int i = 0; i < 9; i++)
+    	{
+    		board[i].getSpace;
+    		counter += i;
+    	}
+        assertEquals(counter,0);
+    }
+}
