c1c5b82e31ed18fb4301d6516d68e91bcb07b06e
frostib <frostib@hotmail.com>
2013-11-24 04:54:37 +0000

    Update SpaceTest.java

--- src/test/java/tictactoe/SpaceTest.java
+++ src/test/java/tictactoe/SpaceTest.java
@@ -13,5 +13,37 @@ public class SpaceTest
         ptest.assigned = 0;
         assertEquals(stest.place(ptest),true);
     }
+    @Test
+    public void setSpaceTest(Placeholder mark)
+    {
+        /* to be implemented
+        this.mark = mark;
+        */
+    }
+
+    // get space
+    @Test
+    public int getSpaceTest()
+    {
+        /* to be implemented
+        return mark.assigned;
+        */
+    }
+
+    // Mark space if it's not taken
+    @Test
+    public boolean place(Placeholder sign)
+    {
+        /* to be implemented
+        if (!(taken))
+        {
+            mark = sign;
+            taken = true;
+            return taken;
+        }
+        return false;
+        */
+    }
+
 }
 
