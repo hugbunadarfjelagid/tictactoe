61271eadc310500e7db0d289fc301e5efe77bd98
frostib <frostib@hotmail.com>
2013-11-24 04:51:16 +0000

    Create PlaceholderTest.java

---
+++ src/test/java/tictactoe/PlaceholderTest.java
@@ -0,0 +1,11 @@
+package tictactoe;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class PlaceholderTest
+{
+    /* to be implemented
+    public int assigned = 0; // assign empty space
+    */
+}
